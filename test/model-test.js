086f615515a16e7ea9c39a1c1dfe16f375d0d176
Susan Dreher <sdreher@columbia.edu>
2016-07-30 09:10:18 -0400

    move imagemapchart into a common npm module

--- test/model-test.js
+++ test/model-test.js
@@ -2,16 +2,7 @@
 
 var assert = require('chai').assert;
 
-var models = require('../src/models.js');
+// var models = require('../src/models.js');
 
 describe('Models', function() {
-    it('ItemList', function() {
-        var data = [{'name': 'item'}];
-        var a = new models.ItemList(data);
-        var output = a.toTemplate();
-
-        assert.equal(output.length, 1);
-        assert.equal(data[0].name, output[0].name);
-    });
-
 });
