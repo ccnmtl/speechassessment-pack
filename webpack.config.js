786fdb233738cda5e54ee97bb007a53c2fea0f80
Susan Dreher <sdreher@columbia.edu>
2018-03-07 14:32:03 -0500

    Remove url-loader

--- webpack.config.js
+++ webpack.config.js
@@ -23,13 +23,13 @@ module.exports = {
             {
                 test: /\.(png|svg|jpg|gif)$/,
                 use: [
-                    'url-loader'
+                    'file-loader'
                 ]
             },
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
                 use: [
-                    'url-loader'
+                    'file-loader'
                 ]
             }
         ]
